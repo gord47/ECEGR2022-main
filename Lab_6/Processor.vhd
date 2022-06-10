ef0d2a45f98bf748aec0f6b5ad6c91c9d56323b7
gord47 <97202987+gord47@users.noreply.github.com>
2022-06-10 15:22:14 -0700

    Lab 6 going out

--- Lab_6/Processor.vhd
+++ Lab_6/Processor.vhd
@@ -84,8 +84,92 @@ architecture holistic of Processor is
 			dataout: out std_logic_vector(31 downto 0);
 			co: out std_logic);
 	end component adder_subtracter;
+	
 
+	signal writeIn: std_logic;
+	signal pcout: std_logic_vector(31 downto 0);
+	signal instr: std_logic_vector(31 downto 0);
+	signal readReg1, readReg2, destReg: std_logic_vector(4 downto 0);
+	signal co1, co2, co3: std_logic;
+	signal Immgenout: std_logic_vector(31 downto 0);
+	signal rightAdder, leftAdder: std_logic_vector(31 downto 0);
+	signal signExt: std_logic_vector(19 downto 0);
+	--Control
+	signal branch: std_logic_vector(1 downto 0);
+	signal MemRead: std_logic;
+	signal MemReg: std_logic;
+	signal ALUCtrl: std_logic_vector(4 downto 0);
+	signal MemWrite: std_logic;
+	signal ALUSrc: std_logic;
+	signal regWrite: std_logic;
+	signal immGen: std_logic_vector(1 downto 0);
+	--ALU
+	signal ALURes: std_logic_vector(31 downto 0);
+	signal ALUintwo: std_logic_vector(31 downto 0);
+	signal isZero: std_logic;
+	--Memory
+	signal dataMem: std_logic_vector(31 downto 0);
+	signal memMuxOut: std_logic_vector(31 downto 0);
+	signal newMemAddress: std_logic_vector(29 downto 0);
+	signal tempo: std_logic_vector(31 downto 0);
+	--Registers
+	signal readout1: std_logic_vector(31 downto 0);
+	signal readout2: std_logic_vector(31 downto 0);
+	--Branch
+	signal brancher: std_logic;
+	signal branchSelect: std_logic_vector(2 downto 0);
+	signal branchRes: std_logic_vector(31 downto 0);
 begin
+	
+	readReg1 <= instr(19 downto 15);
+	readReg2 <= instr(24 downto 20);
+	destReg <= instr(11 downto 7);
 	-- Add your code here
+	writeIn <= '1' when (falling_edge(clock) and regWrite = '1') else
+		'0';
+	--determining immediate value
+	with instr(31) select
+	signExt <= (others => '1') when '1',
+	(others => '0') when others;
+	
+	with immGen select
+	Immgenout <= (signExt(19 downto 0) & instr(31 downto 20)) when "10", --I type
+		(signExt(18 downto 0) & instr(31) & instr(7) & instr(30 downto 25) & instr(11 downto 8) & '0') when "01", --B type
+		(signExt(19 downto 0) & instr(31 downto 25) & instr(11 downto 7)) when "11", --S
+		(instr(31 downto 12) & "000000000000") when others; --U
+	--for branch
+	branchSelect <= isZero & branch;
+	with branchSelect select
+	brancher <= '1' when "001", --BNE
+		'0' when "101",
+		'1' when "110", --BEQ
+		'0' when "010",
+		'0' when others;
+
+	PC: ProgramCounter port map(reset, clock, branchRes, pcout);
+
+	InstructionMemory: InstructionRAM port map(reset, clock, pcout(31 downto 2), instr);
+
+	ctrl: Control port map(clock, instr(6 downto 0), instr(14 downto 12), instr(31 downto 25), branch, memRead, memReg, ALUCtrl, MemWrite, ALUSrc, regWrite, immGen);
+
+	reg: Registers port map(readReg1, readReg2, destReg, memMuxOut, writeIn, readout1, readout2);
+
+	Reg2AluMUX: BusMux2to1 port map(ALUSrc, readout2, Immgenout, ALUintwo);
+
+	ALUModule: ALU port map(readout1, ALUintwo, ALUCtrl, isZero, ALURes);
+
+	offsetSubtractor: adder_subtracter port map(ALURes, x"10000000",'1', tempo, co3);
+
+	newMemaddress <= tempo(31 downto 2);
+
+	DataMemory: RAM port map(reset, clock, MemRead, MemWrite, newMemAddress, readout2, dataMem);
+
+	Data2MemMUX: BusMux2to1 port map(MemReg, ALURes, dataMem, memMuxOut);
+
+	Adder1: adder_subtracter port map(pcout, Immgenout,'1', rightAdder, co1);
+
+	Adder2: adder_subtracter port map(pcout, "00000000000000000000000000000100", '0', leftAdder, co2);
+
+	rightAdderMux: BusMux2to1 port map(brancher,leftAdder, rightAdder, branchRes); 
 end holistic;
 
